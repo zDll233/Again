12c7d91c938043d7b7694a09d516c09ea8bd0114
zdl <zdl951@qq.com>
2024-09-14 22:52:01 +0800

    vk_panel

--- lib/screens/lists/voice_work_panel.dart
+++ lib/screens/lists/voice_work_panel.dart
@@ -4,52 +4,56 @@ import 'package:again/components/future_list.dart';
 import 'package:again/components/vk_menu_btn.dart';
 import 'package:again/components/voice_panel.dart';
 import 'package:again/components/image_thumbnail.dart';
+import 'package:again/presentation/filter/sort_oder/sort_order_state.dart';
+import 'package:again/presentation/u_i_providers.dart';
+import 'package:again/repository/repository_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class VoiceWorkPanel extends StatelessWidget {
+class VoiceWorkPanel extends ConsumerWidget {
   const VoiceWorkPanel({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Obx(() => VoicePanel(
-          title:
-              'VoiceWorks(${c.ui.selectedVkList.length}): ${c.ui.sortOrder.value == SortOrder.byTitle ? 'title' : 'time'}',
-          listView: FutureVoiceWorkListView(),
-          icon: const Icon(Icons.refresh),
-          onIconBtnPressed: c.db.onUpdatePressed,
-          onTextBtnPressed: c.ui.onSortOrderPressed,
-        ));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return VoicePanel(
+      title:
+          'VoiceWorks(${ref.watch(voiceWorkProvider).values.length}): ${ref.watch(sortOrderProvider).selectedItem == SortOrder.byTitle ? 'title' : 'time'}',
+      listView: const FutureVoiceWorkListView(),
+      icon: const Icon(Icons.refresh),
+      onIconBtnPressed: ref.read(repositoryProvider.notifier).onUpdatePressed,
+      // TODO: 没传参数?
+      onTextBtnPressed: () => ref.read(sortOrderProvider.notifier).onSelected,
+    );
   }
 }
 
-class FutureVoiceWorkListView extends StatelessWidget {
+class FutureVoiceWorkListView extends ConsumerWidget {
   const FutureVoiceWorkListView({super.key});
 
-
-  Future<List> fetchItems() async {
-    return c.ui.selectedVkList.toList();
+  Future<List> fetchItems(WidgetRef ref) async {
+    return ref.watch(voiceWorkProvider).values;
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Obx(() => FutureListView(
-          future: fetchItems(),
-          itemBuilder: (context, vk, index) {
-            return Obx(() => ListTile(
-                  leading: ImageThumbnail(imagePath: vk.coverPath),
-                  title: Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 5.0),
-                    child: Text(vk.title),
-                  ),
-                  trailing: VkMenuBtn(voiceWork: vk, selectedIndex: index),
-                  onTap: () => c.ui.onVkSelected(index),
-                  selected: c.ui.selectedVkIdx.value == index,
-                  contentPadding: const EdgeInsets.only(
-                      top: 5.0, bottom: 5.0, left: 20.0, right: 10.0),
-                  horizontalTitleGap: 0.0,
-                ));
-          },
-          itemScrollController: c.ui.vkScrollController,
-        ));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FutureListView(
+      future: fetchItems(ref),
+      itemBuilder: (context, vk, index) {
+        return ListTile(
+          leading: ImageThumbnail(imagePath: vk.coverPath),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 5.0),
+            child: Text(vk.title),
+          ),
+          trailing: VkMenuBtn(voiceWork: vk, selectedIndex: index),
+          onTap: () => ref.read(voiceWorkProvider.notifier).onSelected(index),
+          selected: ref.watch(voiceWorkProvider).selectedIndex == index,
+          contentPadding: const EdgeInsets.only(
+              top: 5.0, bottom: 5.0, left: 20.0, right: 10.0),
+          horizontalTitleGap: 0.0,
+        );
+      },
+      itemScrollController: ref.read(uiServiceProvider).vkScrollController,
+    );
   }
 }
